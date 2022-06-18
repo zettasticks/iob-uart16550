b8c747e1f5cc66271cb6bcac8544f46f2d2886e7
Pedro Antunes <pedronmantunes@gmail.com>
2022-06-18 01:35:34 +0100

    move uart_defines directory;

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -17,7 +17,7 @@ VHDR+=iob_uart16550_swreg_gen.vh iob_uart16550_swreg_def.vh
 VHDR+=$(LIB_DIR)/hardware/include/iob_lib.vh
 
 #hardware include dirs
-INCLUDE+=$(incdir). $(incdir)$(UART16550_SRC_DIR) $(incdir)$(UART16550_INC_DIR) $(incdir)$(LIB_DIR)/hardware/include
+INCLUDE+=$(incdir). $(incdir)$(UART16550_INC_DIR) $(incdir)$(LIB_DIR)/hardware/include
 
 #sources
 VSRC+=$(UART16550_SRC_DIR)/*.v
