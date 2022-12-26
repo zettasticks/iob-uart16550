17bcb9ca687856efbe73eef3b90e27d5801157e1
Pedro Antunes <pedronmantunes@gmail.com>
2022-12-26 03:01:32 +0000

    Simulation: create uart16550 testbench
    The repository old and unecessary files were also deleted. Started preparing the uart16550 delivery.

--- config.mk
+++ config.mk
@@ -5,23 +5,8 @@ TOP_MODULE=iob_uart16550
 #PATHS
 REMOTE_ROOT_DIR ?=sandbox/iob-uart16550
 SIM_DIR ?=$(UART16550_HW_DIR)/simulation
-FPGA_DIR ?=$(shell find $(UART16550_DIR)/hardware -name $(FPGA_FAMILY))
-DOC_DIR ?=$(UART16550_DIR)/document/$(DOC)
 
-LIB_DIR ?=$(UART16550_DIR)/submodules/LIB
 UART16550_HW_DIR:=$(UART16550_DIR)/hardware
-
-#MAKE SW ACCESSIBLE REGISTER
-MKREGS:=$(shell find $(LIB_DIR) -name mkregs.py)
-
-#DEFAULT FPGA FAMILY AND FAMILY LIST
-FPGA_FAMILY ?=CYCLONEV-GT
-FPGA_FAMILY_LIST ?=CYCLONEV-GT XCKU
-
-#DEFAULT DOC AND doc LIST
-DOC ?=pb
-DOC_LIST ?=pb ug
-
 
 # default target
 default: sim
--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -13,13 +13,13 @@ USE_NETLIST ?=0
 
 #include files
 VHDR+=$(wildcard $(UART16550_INC_DIR)/*.vh)
-VHDR+=$(LIB_DIR)/hardware/include/iob_lib.vh
 
 #hardware include dirs
-INCLUDE+=$(incdir). $(incdir)$(UART16550_INC_DIR) $(incdir)$(LIB_DIR)/hardware/include
+INCLUDE+=$(incdir). $(incdir)$(UART16550_INC_DIR)
 
 #sources
-VSRC+=$(UART16550_SRC_DIR)/uart_wb.v $(UART16550_SRC_DIR)/uart_transmitter.v $(UART16550_SRC_DIR)/uart_top.v $(UART16550_SRC_DIR)/uart_tfifo.v $(UART16550_SRC_DIR)/uart_sync_flops.v $(UART16550_SRC_DIR)/uart_rfifo.v $(UART16550_SRC_DIR)/uart_regs.v $(UART16550_SRC_DIR)/uart_receiver.v $(UART16550_SRC_DIR)/uart_debug_if.v $(UART16550_SRC_DIR)/raminfr.v $(UART16550_SRC_DIR)/iob_uart16550.v
+VSRC+=$(wildcard $(UART16550_SRC_DIR)/*.v)
+#$(UART16550_SRC_DIR)/uart_wb.v $(UART16550_SRC_DIR)/uart_transmitter.v $(UART16550_SRC_DIR)/uart_top.v $(UART16550_SRC_DIR)/uart_tfifo.v $(UART16550_SRC_DIR)/uart_sync_flops.v $(UART16550_SRC_DIR)/uart_rfifo.v $(UART16550_SRC_DIR)/uart_regs.v $(UART16550_SRC_DIR)/uart_receiver.v $(UART16550_SRC_DIR)/uart_debug_if.v $(UART16550_SRC_DIR)/raminfr.v $(UART16550_SRC_DIR)/iob_uart16550.v
 
 uart16550-hw-clean: uart16550-gen-clean
 	@rm -f *.v *.vh
