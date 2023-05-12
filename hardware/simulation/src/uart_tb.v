d9741c110ca3f92800849fd9dcdf4710bdbd332a
Pedro Antunes <pedronmantunes@gmail.com>
2023-05-12 17:32:22 +0200

    uart_tb.v: Use named ports for module instantiation with more than one port [Style: module-instantiation] [module-port]

--- hardware/simulation/src/uart_tb.v
+++ hardware/simulation/src/uart_tb.v
@@ -61,38 +61,68 @@ localparam BYTE_1 = 8'b10000001;
 localparam BYTE_2 = 8'b01000010;
 
 iob_uart16550_sim_wrapper uart_snd(
-    clk,
+    .clk(clk),
     
     // Wishbone signals
-    wb_rst_i, wb_adr_i, wb_dat_i, wb_dat_o, wb_we_i, wb_stb_i, wb_cyc_i, wb_ack_o,  wb_sel_i,
-    int_o, // interrupt request
+    .wb_rst_i(wb_rst_i), 
+    .wb_adr_i(wb_adr_i), 
+    .wb_dat_i(wb_dat_i), 
+    .wb_dat_o(wb_dat_o), 
+    .wb_we_i(wb_we_i), 
+    .wb_stb_i(wb_stb_i), 
+    .wb_cyc_i(wb_cyc_i), 
+    .wb_ack_o(wb_ack_o),  
+    .wb_sel_i(wb_sel_i),
+    // interrupt request
+    .int_o(int_o), 
     
     // UART signals
     // serial input/output
-    pad_stx_o, pad_srx_i,
+    .pad_stx_o(pad_stx_o), 
+    .pad_srx_i(pad_srx_i),
     
     // modem signals
-    rts_o, cts_i, dtr_o, dsr_i, ri_i, dcd_i
+    .rts_o(rts_o), 
+    .cts_i(cts_i), 
+    .dtr_o(dtr_o), 
+    .dsr_i(dsr_i), 
+    .ri_i(ri_i), 
+    .dcd_i(dcd_i)
 `ifdef UART_HAS_BAUDRATE_OUTPUT
-    , baud1_o
+    , .baud1_o(baud1_o)
 `endif
 );
 
 iob_uart16550_sim_wrapper uart_rcv(
-  clk, 
+  .clk(clk), 
   
   // Wishbone signals
-  wb_rst_i, wb1_adr_i, wb1_dat_i, wb1_dat_o, wb1_we_i, wb1_stb_i, wb1_cyc_i, wb1_ack_o, wb1_sel_i,  
-  int1_o, // interrupt request
+  .wb_rst_i(wb_rst_i), 
+  .wb_adr_i(wb1_adr_i), 
+  .wb_dat_i(wb1_dat_i), 
+  .wb_dat_o(wb1_dat_o), 
+  .wb_we_i(wb1_we_i), 
+  .wb_stb_i(wb1_stb_i), 
+  .wb_cyc_i(wb1_cyc_i), 
+  .wb_ack_o(wb1_ack_o), 
+  .wb_sel_i(wb1_sel_i),  
+  // interrupt request
+  .int_o(int1_o), 
 
   // UART signals
   // serial input/output
-  stx1_o, srx1_i,
+  .pad_stx_o(stx1_o), 
+  .pad_srx_i(srx1_i),
 
   // modem signals
-  rts1_o, cts1_i, dtr1_o, dsr1_i, ri1_i, dcd1_i
+  .rts_o(rts1_o), 
+  .cts_i(cts1_i), 
+  .dtr_o(dtr1_o), 
+  .dsr_i(dsr1_i), 
+  .ri_i(ri1_i), 
+  .dcd_i(dcd1_i)
 `ifdef UART_HAS_BAUDRATE_OUTPUT
-  , baud2_o
+  , .baud1_o(baud2_o)
 `endif
 
   );
